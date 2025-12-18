88f5d98eed6e946482ac4058c0cccc9dcbca2c35
MikhailTravin <78166836+MikhailTravin@users.noreply.github.com>
2025-12-18 04:08:45 +0300

    Add files via upload

--- dist/js/scripts.js
+++ dist/js/scripts.js
@@ -1,4 +1,6 @@
 document.addEventListener('DOMContentLoaded', function () {
+
+
   // SLIDERS
   new Swiper('.main__slider', {
     slidesPerView: 1,
@@ -18,9 +20,43 @@ document.addEventListener('DOMContentLoaded', function () {
     slidesPerView: 1,
     spaceBetween: 0,
     loop: true,
+    autoplay: {
+      delay: 3000
+    },
+    speed: 1000,
     pagination: {
       el: '.values__pagination',
       clickable: true,
+    },
+    on: {
+      init: function () {
+        updateSliderTheme(this);
+      },
+      slideChange: function () {
+        updateSliderTheme(this);
+      },
+      slideChangeTransitionEnd: function () {
+        updateSliderTheme(this);
+      }
+    }
+  });
+
+  new Swiper('.block-interested__slider', {
+    slidesPerView: 1.1,
+    spaceBetween: 10,
+    breakpoints: {
+      500: {
+        slidesPerView: 1.3,
+        spaceBetween: 20,
+      },
+      992: {
+        slidesPerView: 1.5,
+        spaceBetween: 20,
+      },
+      1300: {
+        slidesPerView: 2.2,
+        spaceBetween: 20,
+      },
     },
     on: {
       init: function () {
@@ -139,49 +175,57 @@ document.addEventListener('DOMContentLoaded', function () {
   // END SCROLL TOP
 
   // RANGE SLIDERS
-  function initRangeSlider(sliderId, minId, maxId, options) {
-    const slider = document.getElementById(sliderId);
-    const minInput = document.getElementById(minId);
-    const maxInput = document.getElementById(maxId);
+  function initRangeSliders() {
+    // Находим все контейнеры с классом range
+    const rangeContainers = document.querySelectorAll('.range');
 
-    if (!slider || !minInput || !maxInput) return;
+    rangeContainers.forEach(container => {
+      const sliderElement = container.querySelector('.slider');
+      const minInput = container.querySelector('.range__input-min');
+      const maxInput = container.querySelector('.range__input-max');
 
-    noUiSlider.create(slider, {
-      start: [options.min, options.max],
-      connect: true,
-      range: {
-        'min': options.min,
-        'max': options.max
-      }
-    });
+      if (!sliderElement || !minInput || !maxInput) return;
 
-    slider.noUiSlider.on('update', function (values, handle) {
-      const value = Math.round(values[handle]);
-      if (handle) {
-        maxInput.value = value;
-      } else {
-        minInput.value = value;
-      }
-    });
+      // Получаем значения из data-атрибутов или из инпутов
+      const minValue = parseInt(sliderElement.getAttribute('data-min') || minInput.value);
+      const maxValue = parseInt(sliderElement.getAttribute('data-max') || maxInput.value);
 
-    minInput.addEventListener('change', function () {
-      slider.noUiSlider.set([this.value, null]);
-    });
+      // Создаем слайдер
+      const sliderInstance = noUiSlider.create(sliderElement, {
+        start: [minInput.value, maxInput.value],
+        connect: true,
+        range: {
+          'min': minValue,
+          'max': maxValue
+        }
+      });
 
-    maxInput.addEventListener('change', function () {
-      slider.noUiSlider.set([null, this.value]);
+      // Обновляем инпуты при движении слайдера
+      sliderInstance.on('update', function (values, handle) {
+        const value = Math.round(values[handle]);
+        if (handle) {
+          maxInput.value = value;
+        } else {
+          minInput.value = value;
+        }
+      });
+
+      // Обновляем слайдер при изменении инпутов
+      minInput.addEventListener('change', function () {
+        const value = Math.max(minValue, Math.min(this.value, maxValue));
+        this.value = value;
+        sliderInstance.set([value, null]);
+      });
+
+      maxInput.addEventListener('change', function () {
+        const value = Math.max(minValue, Math.min(this.value, maxValue));
+        this.value = value;
+        sliderInstance.set([null, value]);
+      });
     });
   }
 
-  initRangeSlider('priceRange', 'priceRangeFrom', 'priceRangeTo', {
-    min: 15,
-    max: 315
-  });
-
-  initRangeSlider('sizeRange', 'sizeRangeFrom', 'sizeRangeTo', {
-    min: 7,
-    max: 359
-  });
+  initRangeSliders();
   // END RANGE SLIDERS
 
   // POPUP
@@ -711,7 +755,7 @@ document.addEventListener('DOMContentLoaded', function () {
     };
   }
 
-  if (document.getElementById('map') || document.getElementById('map2')) {
+  if (document.getElementById('map') || document.getElementById('map2') || document.getElementById('map3')) {
     showYaMaps();
   }
 
@@ -735,42 +779,468 @@ document.addEventListener('DOMContentLoaded', function () {
       map.behaviors.disable('scrollZoom');
       map.geoObjects.add(placemark);
     }
-
     if (document.getElementById('map2')) {
       var map2 = new ymaps.Map("map2", {
-        center: [55.7643235689689, 37.55611849999994],
-        zoom: 17,
-        controls: []
+        center: [55.7643, 37.5561],
+        zoom: 15,
       });
 
-      var placemark2 = new ymaps.Placemark([55.7643235689689, 37.55611849999994], {
-        hintContent: '123022 Звенигородское ш. 18/20, стр. 1'
-      }, {
-        iconLayout: 'default#image',
-        iconImageHref: 'images/mark.svg',
-        iconImageSize: [46, 46],
-        iconImageOffset: [-23, -46]
+      var clusterer = new ymaps.Clusterer({
+        clusterDisableClickZoom: false,
+        clusterOpenBalloonOnClick: false,
+        hasBalloon: false,
+        clusterIcons: [{
+          href: 'img/cluster.svg',
+          size: [56, 56],
+          offset: [-28, -28]
+        }],
+        clusterIconContentLayout: ymaps.templateLayoutFactory.createClass(
+          '<div class="cluster-number">$[properties.geoObjects.length]</div>'
+        )
       });
 
+      var clusterCoords = [
+        [55.7643, 37.5561],
+        [55.7650, 37.5560],
+        [55.7635, 37.5550]
+      ];
+
+      for (var i = 0; i < clusterCoords.length; i++) {
+        var placemark = new ymaps.Placemark(
+          clusterCoords[i],
+          {},
+          {
+            iconLayout: 'default#image',
+            iconImageHref: 'images/mark.svg',
+            iconImageSize: [30, 30],
+            iconImageOffset: [-15, -15],
+            hasBalloon: false,
+            hasHint: false
+          }
+        );
+        clusterer.add(placemark);
+      }
+
+      var customIconLayout = ymaps.templateLayoutFactory.createClass(
+        '<a href="#" class="custom-marker">' +
+        '<div class="marker-header">' +
+        '<img class="marker-icon" src="{{ properties.markerData.icon }}" alt="">' +
+        '</div>' +
+        '<div class="marker-body">' +
+        '<div class="marker-address">{{ properties.markerData.address }}</div>' +
+        '<div class="marker-details">' +
+        '<span class="marker-area">{{ properties.markerData.area }}</span>' +
+        '<span class="marker-tenant">{{ properties.markerData.tenant }}</span>' +
+        '</div>' +
+        '</div>' +
+        '</a>'
+      );
+
+      var htmlMarkersData = [
+        {
+          coords: [55.763690, 37.602005],
+          data: {
+            icon: 'images/objects/list/item-1.jpg',
+            address: 'Большая Бронная, 25с3',
+            area: '46,5 м2',
+            tenant: 'Арендатор: THE KAS',
+          }
+        },
+      ];
+
+      for (var i = 0; i < htmlMarkersData.length; i++) {
+        var marker = htmlMarkersData[i];
+
+        var htmlPlacemark = new ymaps.Placemark(
+          marker.coords,
+          {
+            markerData: marker.data
+          },
+          {
+            iconLayout: customIconLayout,
+            iconOffset: [0, -120],
+            iconShape: {
+              type: 'Rectangle',
+              coordinates: [[-100, -140], [100, 20]]
+            },
+            hasBalloon: false,
+            hasHint: false,
+            hideIconOnBalloonOpen: false
+          }
+        );
+
+        map2.geoObjects.add(htmlPlacemark);
+      }
+
+      map2.geoObjects.add(clusterer);
       map2.behaviors.disable('scrollZoom');
-      map2.geoObjects.add(placemark2);
+
+      clusterer.events.add('click', function (e) {
+        var target = e.get('target');
+        if (target instanceof ymaps.Cluster) {
+          map2.setBounds(target.getBounds(), {
+            checkZoomRange: true,
+            zoomMargin: 30
+          });
+        }
+      });
+    }
+
+    if (document.getElementById('map3')) {
+      var map3 = new ymaps.Map("map3", {
+        center: [55.763690, 37.602005],
+        zoom: 15,
+      });
+
+      var customIconLayout = ymaps.templateLayoutFactory.createClass(
+        '<a href="#" class="custom-marker">' +
+        '<div class="marker-header">' +
+        '<img class="marker-icon" src="{{ properties.markerData.icon }}" alt="">' +
+        '</div>' +
+        '<div class="marker-body">' +
+        '<div class="marker-address">{{ properties.markerData.address }}</div>' +
+        '<div class="marker-details">' +
+        '<span class="marker-area">{{ properties.markerData.area }}</span>' +
+        '<span class="marker-tenant">{{ properties.markerData.tenant }}</span>' +
+        '</div>' +
+        '</div>' +
+        '</a>'
+      );
+
+      var htmlMarkersData = [
+        {
+          coords: [55.763690, 37.602005],
+          data: {
+            icon: 'images/objects/list/item-1.jpg',
+            address: 'Большая Бронная, 25с3',
+            area: '46,5 м2',
+            tenant: 'Арендатор: THE KAS',
+          }
+        },
+      ];
+
+      for (var i = 0; i < htmlMarkersData.length; i++) {
+        var marker = htmlMarkersData[i];
+
+        var htmlPlacemark = new ymaps.Placemark(
+          marker.coords,
+          {
+            markerData: marker.data
+          },
+          {
+            iconLayout: customIconLayout,
+            iconOffset: [0, -120],
+            iconShape: {
+              type: 'Rectangle',
+              coordinates: [[-100, -140], [100, 20]]
+            },
+            hasBalloon: false,
+            hasHint: false,
+            hideIconOnBalloonOpen: false
+          }
+        );
+
+        map3.geoObjects.add(htmlPlacemark);
+      }
+
+      map3.behaviors.disable('scrollZoom');
     }
   }
   // END YANDEX MAP
 });
 
-//========================================================================================================================================================
+if (document.querySelector('.images-product')) {
+  const thumbsSwiper = new Swiper('.images-product__thumb', {
+    observer: true,
+    observeParents: true,
+    slidesPerView: 7,
+    spaceBetween: 12,
+    speed: 400,
+    preloadImages: true,
+    breakpoints: {
+      992: {
+        slidesPerView: 4.9, spaceBetween: 12,
+      },
+      1200: {
+        slidesPerView: 6.9, spaceBetween: 12,
+      },
+    },
+  });
 
-const btnFilter = document.querySelector('.btn-filter');
-const filterClose = document.querySelector('.filter-top__close');
-
-if (btnFilter) {
-  btnFilter.addEventListener('click', function () {
-    document.documentElement.classList.add('filter-open');
+  const mainThumbsSwiper = new Swiper('.images-product__slider', {
+    thumbs: {
+      swiper: thumbsSwiper
+    },
+    observer: true,
+    observeParents: true,
+    slidesPerView: 1,
+    spaceBetween: 0,
+    speed: 400,
+    preloadImages: true,
+    pagination: {
+      el: '.images-product__pagination',
+      clickable: true,
+    },
+    navigation: {
+      prevEl: '.images-product__arrow-prev',
+      nextEl: '.images-product__arrow-next',
+    },
   });
 }
-if (filterClose) {
-  filterClose.addEventListener('click', function () {
-    document.documentElement.classList.remove('filter-open');
+
+Fancybox.bind("[data-fancybox]", {
+  // опции
+});
+
+//========================================================================================================================================================
+
+document.addEventListener('click', function (e) {
+  const target = e.target;
+  const btn = target.closest('[data-open-filter]');
+
+  if (btn) {
+    e.preventDefault();
+    const filterType = btn.getAttribute('data-open-filter');
+    let filterElement = null;
+
+    if (filterType === 'main') {
+      filterElement = document.querySelector('[data-filter-id="main-filter"]');
+    } else if (filterType === 'catalog') {
+      filterElement = document.querySelector('.catalog-filter');
+    } else if (filterType === 'map') {
+      filterElement = document.querySelector('.filter-map');
+    }
+
+    if (filterElement) {
+      filterElement.classList.add('filter-open');
+      document.documentElement.classList.add('filter_open');
+    }
+  }
+
+  if (target.closest('.filter-top__close') || target.closest('.filter-map__close')) {
+    document.querySelectorAll('.filter-open').forEach(el => {
+      el.classList.remove('filter-open');
+    });
+    document.documentElement.classList.remove('filter_open');
+  }
+
+});
+
+const btnMap = document.querySelector('.btn-map');
+const btnCloseMap = document.querySelector('.filter-map__close');
+if (btnMap) {
+  btnMap.addEventListener('click', function (event) {
+    event.preventDefault();
+    document.documentElement.classList.add('map-open');
   });
+  btnCloseMap.addEventListener('click', function () {
+    document.documentElement.classList.remove('map-open');
+  });
+}
+
+const layoutContainers = document.querySelectorAll('.block-layout__content');
+
+if (layoutContainers) {
+  layoutContainers.forEach(container => {
+    const zoomContainer = container.querySelector('.block-layout__zoom-container');
+    const image = container.querySelector('.block-layout__image');
+    const zoomPlusBtn = container.querySelector('.zoom-plus');
+    const zoomMinusBtn = container.querySelector('.zoom-minus');
+
+    // Настройки зума
+    let currentScale = 1;
+    const minScale = 0.5;
+    const maxScale = 3;
+    const scaleStep = 0.25;
+
+    let isDragging = false;
+    let startX, startY;
+    let translateX = 0;
+    let translateY = 0;
+
+    function getCenter() {
+      const containerRect = zoomContainer.getBoundingClientRect();
+      return {
+        x: containerRect.width / 2,
+        y: containerRect.height / 2
+      };
+    }
+
+    function centerImage() {
+      const imageRect = image.getBoundingClientRect();
+      const center = getCenter();
+
+      translateX = 0;
+      translateY = 0;
+      updateTransform();
+    }
+
+    function updateTransform() {
+      image.style.transform = `
+                translate(${translateX}px, ${translateY}px)
+                scale(${currentScale})
+            `;
+    }
+
+    function updateScale(newScale) {
+      const oldScale = currentScale;
+      currentScale = Math.max(minScale, Math.min(maxScale, newScale));
+
+      if (currentScale === 1) {
+        centerImage();
+      } else if (currentScale !== oldScale && currentScale > 1) {
+        const scaleChange = currentScale / oldScale;
+        translateX *= scaleChange;
+        translateY *= scaleChange;
+      }
+
+      constrainPosition();
+      updateTransform();
+
+      zoomPlusBtn.disabled = currentScale >= maxScale;
+      zoomMinusBtn.disabled = currentScale <= minScale;
+
+      zoomContainer.style.cursor = currentScale > 1 ? 'grab' : 'default';
+    }
+
+    function constrainPosition() {
+      if (currentScale <= 1) {
+        translateX = 0;
+        translateY = 0;
+        return;
+      }
+
+      const containerRect = zoomContainer.getBoundingClientRect();
+      const imageRect = image.getBoundingClientRect();
+
+      const scaledWidth = imageRect.width * currentScale;
+      const scaledHeight = imageRect.height * currentScale;
+
+      const maxMoveX = (scaledWidth - containerRect.width) / 2;
+      const maxMoveY = (scaledHeight - containerRect.height) / 2;
+
+      translateX = Math.max(-maxMoveX, Math.min(maxMoveX, translateX));
+      translateY = Math.max(-maxMoveY, Math.min(maxMoveY, translateY));
+
+      if (scaledWidth <= containerRect.width) {
+        translateX = 0;
+      }
+      if (scaledHeight <= containerRect.height) {
+        translateY = 0;
+      }
+    }
+
+    zoomContainer.addEventListener('mousedown', startDrag);
+
+    function startDrag(e) {
+      if (currentScale <= 1) return;
+
+      e.preventDefault();
+      isDragging = true;
+      startX = e.clientX - translateX;
+      startY = e.clientY - translateY;
+      zoomContainer.style.cursor = 'grabbing';
+
+      document.addEventListener('mousemove', drag);
+      document.addEventListener('mouseup', stopDrag);
+    }
+
+    function drag(e) {
+      if (!isDragging) return;
+
+      translateX = e.clientX - startX;
+      translateY = e.clientY - startY;
+
+      constrainPosition();
+      updateTransform();
+    }
+
+    function stopDrag() {
+      isDragging = false;
+      zoomContainer.style.cursor = currentScale > 1 ? 'grab' : 'default';
+
+      document.removeEventListener('mousemove', drag);
+      document.removeEventListener('mouseup', stopDrag);
+    }
+
+    zoomPlusBtn.addEventListener('click', function () {
+      if (currentScale < maxScale) {
+        updateScale(currentScale + scaleStep);
+      }
+    });
+
+    zoomMinusBtn.addEventListener('click', function () {
+      if (currentScale > minScale) {
+        updateScale(currentScale - scaleStep);
+      }
+    });
+
+    zoomContainer.addEventListener('wheel', function (e) {
+      e.preventDefault();
+
+      if (currentScale <= minScale && e.deltaY > 0) return;
+      if (currentScale >= maxScale && e.deltaY < 0) return;
+
+      const rect = zoomContainer.getBoundingClientRect();
+      const mouseX = e.clientX - rect.left;
+      const mouseY = e.clientY - rect.top;
+      const center = getCenter();
+
+      const offsetFromCenterX = mouseX - center.x;
+      const offsetFromCenterY = mouseY - center.y;
+
+      const oldScale = currentScale;
+
+      if (e.deltaY < 0) {
+        updateScale(currentScale + scaleStep);
+      } else {
+        updateScale(currentScale - scaleStep);
+      }
+
+      if (currentScale !== oldScale && currentScale > 1) {
+        const scaleChange = currentScale / oldScale;
+        translateX += offsetFromCenterX * (1 - scaleChange);
+        translateY += offsetFromCenterY * (1 - scaleChange);
+
+        constrainPosition();
+        updateTransform();
+      }
+    }, { passive: false });
+
+    zoomContainer.addEventListener('dblclick', function (e) {
+      e.preventDefault();
+      updateScale(1);
+    });
+
+    centerImage();
+    updateScale(1);
+
+    window.addEventListener('resize', function () {
+      centerImage();
+      constrainPosition();
+      updateTransform();
+    });
+  });
+}
+
+const stickyBlock = document.querySelector('.block-sticky');
+const targetBlock = document.querySelector('.block-descr-product');
+
+if (stickyBlock && targetBlock) {
+  const OFFSET = 100;
+
+  function checkStickyActive() {
+    const targetRect = targetBlock.getBoundingClientRect();
+    const windowHeight = window.innerHeight;
+
+    if (targetRect.bottom < OFFSET) {
+      stickyBlock.classList.add('active');
+    } else {
+      stickyBlock.classList.remove('active');
+    }
+  }
+
+  setTimeout(checkStickyActive, 10);
+
+  window.addEventListener('scroll', checkStickyActive);
 }
